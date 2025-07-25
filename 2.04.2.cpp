825c3fec0bfd36b8773a1a13c5fab4332d6e78e6
Amitav Krishna <amtiavkrishna2011@gmail.com>
2025-07-25 13:29:15 -0400

    I forgored what changes I actually made. Lookatdif

---
+++ 2.04.2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int doubleNumber(int x)
+  {
+    return x * 2;
+  }
+int main()
+  {
+    int x{};
+    std::cout << "Enter a number: ";
+    std::cin >> x;
+    std::cout << x << " doubled is " << doubleNumber(x) << ".\n";
+    return 0;
+  }
