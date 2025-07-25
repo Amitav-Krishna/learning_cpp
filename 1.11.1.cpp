5d48998bfa890a425a93c3c890ef76de2ffa4973
Amitav Krishna <amtiavkrishna2011@gmail.com>
2025-07-24 21:20:53 -0400

    Made program work¯\_(ツ)_/¯

--- 1.11.1.cpp
+++ 1.11.1.cpp
@@ -1,1 +1,13 @@
-#include "1.11.1.h"
+#include <iostream>
+
+
+int main()
+  {
+    std::cout << "Enter an integer: ";
+    int x{ };
+    std::cin >> x;
+    int y{ };
+    int multiplier{ 2 };
+    y = multiplier * x;
+    std::cout <<"Double that number is: " << y << std::endl;
+  }
